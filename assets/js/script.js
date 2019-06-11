78add4e166d6b5d5435fcc1c6deaa01708c03436
Kraus, Simon <simon.kraus@aol.com>
2019-06-11 13:29:39 -0500

    onClick w/ variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,16 @@ console.log("Hello, beautiful");
 $(document).ready(function(){
     console.log("ready!");
 
-    var tFrequency = 3;
+    
+    $("#submit-input").on("click", function(){
+
+        var trainName = $("#train-name").val();
+        var destination =$("#destination").val();
+        var firstTime = $("#first-time").val();
+        var frequency = $("#frequency").val();
+    })
+
+
 
 
 

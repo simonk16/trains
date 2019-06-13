fa9e212d3ee4dc37a403ce5207de767c5137aa99
Kraus, Simon <simon.kraus@aol.com>
2019-06-12 23:02:25 -0500

    Data can now be stored to firebase ^-^

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,48 +1,39 @@
 console.log("Hello, beautiful");
+        
+        // Your web app's Firebase configuration
+        var config = {
+            apiKey: "AIzaSyAMzEcFlp8p6SK9u1hQSp_Kmxv-k0ac9Lo",
+            authDomain: "train-times-d1225.firebaseapp.com",
+            databaseURL: "https://train-times-d1225.firebaseio.com",
+            storageBucket: "train-times-d1225.appspot.com",
+        };
+        
+        // Initialize Firebase
+        firebase.initializeApp(config);
 
-$(document).ready(function(){
-    console.log("ready!");
+        var database = firebase.database();
 
-    
-  // Your web app's Firebase configuration
-    var firebaseConfig = {
-        apiKey: "AIzaSyAMzEcFlp8p6SK9u1hQSp_Kmxv-k0ac9Lo",
-        authDomain: "train-times-d1225.firebaseapp.com",
-        databaseURL: "https://train-times-d1225.firebaseio.com",
-        projectId: "train-times-d1225",
-        storageBucket: "train-times-d1225.appspot.com",
-        messagingSenderId: "305345252541",
-        appId: "1:305345252541:web:f1792f7fcd12fc58"
-    };
-  // Initialize Firebase
-    firebase.initializeApp(firebaseConfig);
-    
-    $("#submit-input").on("click", function(){
+        $("#submit-input").on("click", function(event){
+            event.preventDefault();
+                    
 
-        var trainName = $("#train-name").val();
-        var destination =$("#destination").val();
-        var firstTime = $("#first-time").val();
-        var frequency = $("#frequency").val();
-    })
+            var trainName = $("#train-name").val().trim();
+            var destination = $("#destination").val().trim();
+            var firstTime = $("#first-time").val().trim();
+            var frequency = $("#frequency").val().trim();
 
+            var newTrain = {
+                name: trainName,
+                dest: destination,
+                timeOne: firstTime,
+                freq: frequency
+            };
 
+            database.ref().push(newTrain);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-})
+            console.log(newTrain.name);
+            console.log(newTrain.dest);
+            console.log(newTrain.timeOne);
+            console.log(newTrain.freq);
+            
+        });

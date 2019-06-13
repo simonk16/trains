cd919a7eff4cdf2ee44fb69e871c8e57b8803cc4
Kraus, Simon <simon.kraus@aol.com>
2019-06-12 20:01:54 -0500

    firebare api

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,19 @@ $(document).ready(function(){
     console.log("ready!");
 
     
+  // Your web app's Firebase configuration
+    var firebaseConfig = {
+        apiKey: "AIzaSyAMzEcFlp8p6SK9u1hQSp_Kmxv-k0ac9Lo",
+        authDomain: "train-times-d1225.firebaseapp.com",
+        databaseURL: "https://train-times-d1225.firebaseio.com",
+        projectId: "train-times-d1225",
+        storageBucket: "train-times-d1225.appspot.com",
+        messagingSenderId: "305345252541",
+        appId: "1:305345252541:web:f1792f7fcd12fc58"
+    };
+  // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    
     $("#submit-input").on("click", function(){
 
         var trainName = $("#train-name").val();
